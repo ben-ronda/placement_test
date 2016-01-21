92cc180704661510b4047f90389ab195f5bc7db2
Ben Ronda <bronda95@comcast.net>
2016-01-21 13:20:07 -0800

    alter scripts.js to successfully run pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
-$(document)ready.function() {
-  $("button#ping-pong").click(function() {
-    var userInput = $("#userInput").val();
-    var i;
-    function pingpong(userInput) {
-    for(i = 1; i <= userInput; i++){
-      if (i % 15 == 0) {
-        $("ul").append("<li>" + "ping pong" + "</li>");
-      } 
-      else if (i % 5 == 0) {
-        $("ul").append("<li>" + "pong" + "</li>");
-      } 
-      else if (i % 3 ==0) {
-        $("ul").append("<li>" + "ping" + "</li>");
-      } 
-      else { 
-        $("ul").append("<li>" + i + "</li>");
-      }
+
+function pingpong(userInput) {
+  for(var i = 1; i <= userInput; i++){
+    if (i % 15 === 0) {
+      $("ul").append("<li>" + "ping pong" + "</li>");
+    } 
+    else if (i % 5 === 0) {
+      $("ul").append("<li>" + "pong" + "</li>");
+    } 
+    else if (i % 3 === 0) {
+      $("ul").append("<li>" + "ping" + "</li>");
+    } 
+    else { 
+      $("ul").append("<li>" + i + "</li>");
     }
   }
 }
+
+$(document).ready(function() {
+  $("button#ping-pong").click(function() {
+    $("ul").empty();
+    pingpong($("#userInput").val());
+    event.preventDefault();
+  });
+});
