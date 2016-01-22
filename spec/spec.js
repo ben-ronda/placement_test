2672536a061501f3c378655599fc26752ec0ea06
Ben Ronda <bronda95@comcast.net>
2016-01-21 21:15:06 -0800

    add passing spec to test inputs that are multiples of 5

--- spec/spec.js
+++ spec/spec.js
@@ -2,4 +2,8 @@ describe('pingpong',function() {
   it("should replace a multiple of three with ping", function(){
     expect(pingpong(3)).to.eql([1, 2, "ping"]);
   })
+
+  it("should replace a multiple of 5 with pong", function(){
+    expect(pingpong(5)).to.eql([1, 2, "ping", 4, "pong"]);
+  })
 })
