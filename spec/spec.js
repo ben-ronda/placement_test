050a3bdc7852bf75e496e386715ef3b753af44e1
Ben Ronda <bronda95@comcast.net>
2016-01-21 21:25:12 -0800

    add a passing spec to test inputs that are not mutliple of 3, 5, or both

--- spec/spec.js
+++ spec/spec.js
@@ -9,4 +9,7 @@ describe('pingpong',function() {
   it("should replace a multiple of 15 (3 and 5) with ping pong", function(){
     expect(pingpong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping pong"]);
   })
+  it("should return any number that isn't a multimple of 3, 5, or 15", function(){
+    expect(pingpong(2)).to.eql([1, 2]);
+  })
 })
